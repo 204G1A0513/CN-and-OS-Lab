16c717c263d580acd688f2afc2f7de8cb5011dfd
204G1A0513 <94513176+204G1A0513@users.noreply.github.com>
2022-08-31 20:14:02 +0530

    Commited changes in sample1.c

--- OSlab/Sample1.c
+++ OSlab/Sample1.c
@@ -1,8 +1,8 @@
-/* Name Of the Candidate: L. Ashok
-   Roll No: 204G1A0501
+/* Name Of the Candidate: A.Annapurna
+   Roll No: 204G1A0513
    Title of the Experiment: CPU Scheduling Using FCFS Algorithm
-   Date of Creation: 29-08-2022
-   Date of Execution: 29-08-2022
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
 */
 // Source Code
     #include <stdio.h>
