00bb51aa7f4756bf187fe0ffafcafd38937c8b61
204G1A0513 <204G1A05C5@srit.ac.in>
2022-09-13 12:20:44 +0530

    >Third list of errors are Rectified

--- OSLab/Experiment-1/FCFS.c
+++ OSLab/Experiment-1/FCFS.c
@@ -9,7 +9,7 @@ int main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
-clrscr();
+//clrscr();
 printf("\nEnter the number of processes -- ");
 scanf("%d", &n);
 for(i=0;i<n;i++){
@@ -29,5 +29,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getc();
+//getc();
 }
